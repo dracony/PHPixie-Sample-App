c557ecdcb325ac4198bc1b8b2868920cc99f8e61
dracony <draconyster@gmail.com>
2013-04-24 00:15:19 +0300

    Update main.php

--- assets/views/main.php
+++ assets/views/main.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHPixie polls</title>
+		<title>PHPixie fairies</title>
 		<link href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet">
-		<link href="/style.css" rel="stylesheet">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 	</head>
 	<body>
 		<div class="container">
