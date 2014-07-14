a776613422bfa13ead0586eb431bed1fc05b43d8
Vitaly Martynenko <contact@vitalka.me>
2014-07-14 17:08:18 +0300

    Add alert() to confirm fairy delete action

--- assets/views/view.php
+++ assets/views/view.php
@@ -2,5 +2,5 @@
 <p class="lead">
 	<?php echo $fairy->interests;?>
 </p>
-<a href="/fairies/delete/<?php echo $fairy->id; ?>" class="btn btn-danger">Delete this fairy</a>
+<a href="/fairies/delete/<?php echo $fairy->id; ?>" class="btn btn-danger" onclick="return alert('Are you sure?');">Delete this fairy</a>
 <a href="/" class="btn btn-success">Back to the list of fairies</a>
