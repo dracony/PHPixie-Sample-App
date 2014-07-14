539e3b460bc95edd767e24ca571e9af99970e511
Vitaly Martynenko <contact@vitalka.me>
2014-07-14 16:50:08 +0300

    Add "Delete this fairy" button

--- assets/views/view.php
+++ assets/views/view.php
@@ -2,4 +2,5 @@
 <p class="lead">
 	<?php echo $fairy->interests;?>
 </p>
+<a href="/fairies/delete/<?php echo $fairy->id; ?>" class="btn btn-danger">Delete this fairy</a>
 <a href="/" class="btn btn-success">Back to the list of fairies</a>
